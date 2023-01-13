300747fc736100d16d8725639189a2cb7a83b875
abhayambule <aambule3@gmail.com>
2023-01-13 12:27:15 +0530

    sponsor page design

--- src/components/SponsorCard.js
+++ src/components/SponsorCard.js
@@ -1,11 +1,11 @@
 import { Col } from "react-bootstrap";
 
-export const ProjectCard = ({ title, description, imgUrl }) => {
+export const SponsorCard = ({ title, description, imgUrl }) => {
   return (
     <Col size={12} sm={6} md={4}>
-      <div className="proj-imgbx">
-        <img src={imgUrl} />
-        <div className="proj-txtx">
+      <div className="spon-imgbx">
+        <img className="spon-img" src={imgUrl} />
+        <div className="spon-txtx">
           <h4>{title}</h4>
           <span>{description}</span>
         </div>
